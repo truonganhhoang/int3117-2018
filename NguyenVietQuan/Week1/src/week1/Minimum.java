37bc938c607a557abd9c83bae722f19c214752d6
quannguyen0510 <quannguyen0510@gmail.com>
2018-09-16 15:34:08 +0700

    Fix NguyenVietQuan

--- NguyenVietQuan/Week1/src/week1/Minimum.java
+++ NguyenVietQuan/Week1/src/week1/Minimum.java
@@ -3,26 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Quan;
+package week1;
 
 /**
  *
  * @author Quan
  */
 public class Minimum {
-    public static int findMin(int a[]){
+    public static int findMin(int a[]) {
         int min;
         min = a[0];
-        for(int i = 0; i < a.length; i++){
-            if(min > a[i])
-            min = a[i];
+        for (int i = 0; i < a.length; i++) {
+            if (min > a[i]) {
+                min = a[i];
+            }
         }
         return min;
     }
 }
-
-
-
-
-
-
--- NguyenVietQuan/Week1/test/testMinimum.java
+++ NguyenVietQuan/Week1/test/testMinimum.java
@@ -3,51 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Quan;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import week1.Minimum;
 
 /**
  *
  * @author Quan
  */
-public class MinimumTest {
-    @Test
+public class testMinimum {
+     @Test
     public void MinimumTest() {
-        int a[] = new int[]{4, 1, 5, 7 , 9};
+        int a[] = new int[]{4, 1, 5, 7, 9};
         int expected = 1;
         int result = Minimum.findMin(a);
         assertEquals(expected, result);
     }
-    
+
     @Test
     public void MinimumTest2() {
-        int a[] = new int[]{5, 2, 5, 7 , 9};
+        int a[] = new int[]{5, 2, 5, 7, 9};
         int expected = 2;
         int result = Minimum.findMin(a);
         assertEquals(expected, result);
     }
-    
+
     @Test
     public void MinimumTest3() {
-        int a[] = new int[]{4, 1, 5, 7 , 9};
+        int a[] = new int[]{4, 1, 5, 7, 9};
         int expected = 4;
         int result = Minimum.findMin(a);
         assertEquals(expected, result);
     }
-    
+
     @Test
     public void MinimumTest4() {
-        int a[] = new int[]{4, 12, 5, 7 , 9};
+        int a[] = new int[]{4, 12, 5, 7, 9};
         int expected = 4;
         int result = Minimum.findMin(a);
         assertEquals(expected, result);
     }
-    
+
     @Test
     public void MinimumTest5() {
-        int a[] = new int[]{4, 1, 5, 7 , 9};
+        int a[] = new int[]{4, 1, 5, 7, 9};
         int expected = 9;
         int result = Minimum.findMin(a);
         assertEquals(expected, result);
