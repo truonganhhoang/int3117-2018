6c2917d2b95f753176f81691ea486837afec923e
Hoang Truong <truonganhhoang@gmail.com>
2018-09-17 11:44:41 +0700

    xóa file thừa, đổi tên thư mục cho đúng quy định

---
+++ TrieuHoangAn/Library/TestSuite/AllTests.java
@@ -0,0 +1,11 @@
+package Library.TestSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestProgram.class,TestTray.class })
+public class AllTests {
+
+}
