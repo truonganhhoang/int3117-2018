bf226351821c95e425f90ce6218f953fbe3a0526
daolq98 <42737393+daolq98@users.noreply.github.com>
2018-09-19 23:34:32 +0700

    LeQuangDao

---
+++ LeQuangDao/SoNguyenToTest.java
@@ -0,0 +1,57 @@
+
+package songuyento;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DaoLQ
+ */
+public class SoNguyenToTest {
+    
+    public SoNguyenToTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of SNT method, of class SoNguyenTo.
+     */
+    @Test
+    public void testSNT() {
+        System.out.println("soNguyenTo");
+        int x = 74;
+        SoNguyenTo instance = new SoNguyenTo();
+        boolean expResult = false;
+        boolean result = instance.soNguyenTo(x);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testSNT2() {
+        System.out.println("soNguyenTo");
+        int x = 3;
+        SoNguyenTo instance = new SoNguyenTo();
+        boolean expResult=true;
+        boolean result = instance.soNguyenTo(x);
+        assertEquals(expResult, result);
+    }
+}
