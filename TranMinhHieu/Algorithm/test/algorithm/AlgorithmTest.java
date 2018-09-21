94c3f4d8baa069cca1d99eecb7087c2b13fbe9e0
Tran Minh Hieu <35520308+hieutm2198@users.noreply.github.com>
2018-09-21 23:31:45 +0700

    Update AlgorithmTest.java

--- TranMinhHieu/Algorithm/test/algorithm/AlgorithmTest.java
+++ TranMinhHieu/Algorithm/test/algorithm/AlgorithmTest.java
@@ -1,69 +1,62 @@
 package algorithm;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * This class tests class algorithm.Algorithm
  * @author Tran Minh Hieu
  */
 public class AlgorithmTest {
     
+    /**
+     * Constructor
+     */
     public AlgorithmTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     /**
-     * Test of insertionSort method, of class Algorithm.
+     * Test of insertionSort method, of class Algorithm. Case: Array is empty
      */
     @Test
-    public void testInsertionSort() {
+    public void testInsertionSort1() {
         System.out.println("insertionSort");
         int[] array = null;
         int[] expResult = null;
         int[] result = Algorithm.insertionSort(array);
         assertArrayEquals(expResult, result);
+    }
         
-        int[] array1 = {1, 2, 8, 0, 9, 3, 0};
-        int[] expResult1 = {0, 0, 1, 2, 3, 8, 9};
-        int[] result1 = Algorithm.insertionSort(array1);
-        assertArrayEquals(expResult1, result1);
+    /**
+     * Test of insertionSort method, of class Algorithm. Case: Random
+     */
+    public void testInsertionSort2() {
+        int[] array = {1, 2, 8, 0, 9, 3, 0};
+        int[] expResult = {0, 0, 1, 2, 3, 8, 9};
+        int[] result = Algorithm.insertionSort(array);
+        assertArrayEquals(expResult, result);   
+    }
+    
+    /**
+     * Test of insertionSort method, of class Algorithm. Case: Array has only one element.
+     */
+    public void testInsertionSort3() {
+        int[] array = {1};
+        int[] expResult = {1};
+        int[] result = Algorithm.insertionSort(array);
+        assertArrayEquals(expResult, result);
         
-        int[] array2 = {1};
-        int[] expResult2 = {1};
-        int[] result2 = Algorithm.insertionSort(array2);
-        assertArrayEquals(expResult2, result2);
+    }
+    
+    /**
+     * Test of insertionSort method, of class Algorithm. Case: Array has already been sorted.
+     */
+    public void testInsertionSort4() {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] expResult = {1, 2, 3, 4, 5};
+        int[] result = Algorithm.insertionSort(array);
+        assertArrayEquals(expResult, result);
         
     }
 
-    /**
-     * Test of main method, of class Algorithm.
-     */
-//    @Test
-//    public void testMain() {
-//        System.out.println("main");
-//        String[] args = null;
-//        Algorithm.main(args);
-//       
-//    }
-    
 }
