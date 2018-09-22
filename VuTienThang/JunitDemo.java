b990d1adbd7ceb2923dd5fb4c7f2b6abc6a13ac7
thangvu1234 <37247106+thangvu1234@users.noreply.github.com>
2018-09-22 22:34:08 +0700

    update file

--- VuTienThang/JunitDemo.java
+++ VuTienThang/JunitDemo.java
@@ -1,20 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package junit.demo;
 
-/**
- *
- * @author thang
- */
 public class JunitDemo {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static boolean songuyento(int a){
+    
+    public static boolean camelCase(int a){
         if(a > 0){
             int count = 0;
             for(int i = 1; i <= a; i++){
@@ -27,6 +15,7 @@ public class JunitDemo {
         }
         return false;
     }
+    
     public static void main(String[] args) {
     }
 }
