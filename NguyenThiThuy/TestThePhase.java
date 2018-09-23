0dbdab478ef5916050e163522a4cf337be340282
nguyenthithuy <ntthuyyl@gmail.com>
2018-09-23 09:43:12 +0700

    update code NguyenThiThuy

--- NguyenThiThuy/TestThePhase.java
+++ NguyenThiThuy/TestThePhase.java
@@ -1,25 +1,25 @@
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class TestThePhase extends TestCase{	
-	
+public class TestThePhase extends TestCase{
+
 	public void testMinValue() {
-		ThePhase p new ThePhase();		
+		ThePhase p new ThePhase();
 		assertEquals(0, p.findPhase(Integer.MIN_VALUE));
 	}
 
-	public void testValue0() {
+	public void testZeroValue() {
 		ThePhase p new ThePhase();
-		assertEquals(1, p.findPhase(0));		
+		assertEquals(1, p.findPhase(0));
 	}
 
 	public void testCommonValue() {
 		ThePhase p new ThePhase();
-		assertEquals(1, p.findPhase(1));		
+		assertEquals(1, p.findPhase(1));
 	}
 
 	public void testOverLoad() {
-		ThePhase p new ThePhase();			
+		ThePhase p new ThePhase();
 		assertTrue(0 < p.findPhase(Integer.MAX_VALUE));
 	}
 
