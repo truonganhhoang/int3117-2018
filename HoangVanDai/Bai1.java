b061df1114b9da984775e063f63000e57065d89f
daihv96 <43134817+daihv96@users.noreply.github.com>
2018-09-25 21:04:28 +0700

    Update Bai1.java

--- HoangVanDai/Bai1.java
+++ HoangVanDai/Bai1.java
@@ -14,18 +14,17 @@ public class Bai1 {
      * @param b số thứ hai
      * @return Ước chung lớn nhất của 2 số a, b
      */
-    public int TimUocChungLonNhat(int a, int b) {
-        int Tam = 0;
+    public int timUocChungLonNhat(int a, int b) {
+        int tam = 0;
 
-        if (a == 0 && b == 0) {
+        if ((a == 0) && (b == 0)) {
             System.err.println("Khong co uoc chung lon nhat thoa man");
             return -1;
         } else {
-
             while (b != 0) {
-                Tam = a % b;
+                tam = a % b;
                 a = b;
-                b = Tam;
+                b = tam;
             }
             if (a < 0) {
                 return -a;
@@ -41,14 +40,14 @@ public class Bai1 {
      * @param n là một số nguyên dương bất kỳ
      * @return Fibonacci của số nguyên dương n
      */
-    public int TinhFibonacci(int n) {
+    public int tinhFibonacci(int n) {
         if (n < 0) {
             System.err.println("Dau vao khong hop le");
             return -1;
         } else if (n <= 1) {
             return n;
         } else {
-            return TinhFibonacci(n - 1) + TinhFibonacci(n - 2);
+            return tinhFibonacci(n - 1) + tinhFibonacci(n - 2);
         }
     }
 }
