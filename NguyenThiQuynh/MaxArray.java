875a3991c914fc02b9f0813782da340acb2efd17
layer-quynh <quynhkun15101998@gmail.com>
2018-09-27 12:56:37 +0700

    JUnitTest

--- NguyenThiQuynh/MaxArray.java
+++ NguyenThiQuynh/MaxArray.java
@@ -1,17 +1,20 @@
 package junittest;
 
 /**
- * class nay de tim phan tu lon nhat cua 1 mang cho san
+ * This class find the max element of a given array
  * @author quynh
  */
 public class MaxArray {
+    
     public int findArrayMax(int arr[]) {
         int max = arr[0];
-        for(int i = 0 ; i < arr.length ; i++) {
+        
+        for (int i = 0 ; i < arr.length ; i++) {
             if(arr[i] >= max) {
                 max = arr[i];
             }
         }
+        
         return max;
     }
     
