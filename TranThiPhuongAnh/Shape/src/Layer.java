d1b0a2d279d82f829ef90e377e8b839d1b699cac
TranPhuongAnh <35594710+TranPhuongAnh@users.noreply.github.com>
2018-09-30 22:00:05 +0700

    Update Layer.java

--- TranThiPhuongAnh/Shape/src/Layer.java
+++ TranThiPhuongAnh/Shape/src/Layer.java
@@ -1,12 +1,10 @@
-package Cau3;
-
 import java.util.ArrayList;
 
 /**
  * lop nhieu doi tuong
  * @author Anh Tran
  */
-public class Layer extends Diagram{
+public class Layer extends Diagram {
     //dung sai
     public boolean visible;
     
@@ -14,20 +12,20 @@ public class Layer extends Diagram{
      * tra ve gia tri cua visible
      * @return visible
      */
-    public boolean getVisible(){
+    public boolean getVisible() {
         return visible;
     }
     /**
      * Khoi tao gia tri moi cua visible
      */
-    public void setVisible(){
+    public void setVisible() {
         this.visible = visible;
     }
     
     /**
      * khoi tao mac dinh
      */
-    public Layer(){
+    public Layer() {
         visible = true;
     }
     //tao danh sach Shape
@@ -36,9 +34,9 @@ public class Layer extends Diagram{
     /**
      * Ham xoa tat ca cac hinh tam giac khoi danh sach Shape
      */
-    public void eraseTriangleOfShape(){
-        for(int i=0 ; i<listShape.size() ; i++){
-            if(listShape.get(i) instanceof Triangle){
+    public void eraseTriangleOfShape() {
+        for(int i = 0; i < listShape.size(); i++) {
+            if(listShape.get(i) instanceof Triangle) {
                 listShape.remove(i);
                 
             }
@@ -48,9 +46,9 @@ public class Layer extends Diagram{
     /**
      * Ham xoa tat ca cac hinh tron khoi danh sach Shape
      */
-    public void eraseCircleOfShape(){
-        for(int i=0; i<listShape.size() ; i++){
-            if(listShape.get(i) instanceof Circle){
+    public void eraseCircleOfShape() {
+        for(int i = 0; i < listShape.size(); i++) {
+            if(listShape.get(i) instanceof Circle) {
                 listShape.remove(i);
                 
             }
@@ -61,8 +59,8 @@ public class Layer extends Diagram{
      * Ham xoa tat ca cac hinh co trong Layer
      */
     public void eraseShapeOfLayer(){
-        for(int i=0; i<listShape.size() ; i++){
-            if(listShape.get(i) instanceof Shape){
+        for(int i = 0; i < listShape.size(); i++) {
+            if(listShape.get(i) instanceof Shape) {
                 listShape.remove(i);
             }
         }
@@ -72,9 +70,9 @@ public class Layer extends Diagram{
      * Ham xoa cac hinh trung nhau
      */
     public void eraseSameShape(){
-        for(int i=0; i<listShape.size()-1 ; i++){
-            for(int j=1; j<listShape.size() ; j++){
-                if(listShape.get(i) == listShape.get(j)){
+        for(int i = 0; i < listShape.size() - 1; i++) {
+            for(int j = 1; j < listShape.size(); j++){
+                if(listShape.get(i) == listShape.get(j)) {
                     listShape.remove(j);
                 }
             }
