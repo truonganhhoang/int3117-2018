41d063cfc6c45c6cc6f76e8bff2bbabf242b79d3
DamDinhDinh <xifurifeng@gmail.com>
2018-10-22 11:47:04 +0700

    coverage

--- DamDinhDinh/SumPositiveNumberOfArrayTest.java
+++ DamDinhDinh/SumPositiveNumberOfArrayTest.java
@@ -22,11 +22,19 @@ public class SumPositiveNumberOfArrayTest {
         assertEquals(expect_sum, result);
     }
 
-
     @Test
     public  void testCalculateSumPositiveNumber3() {
         int array[] = {2147483647, Integer.MIN_VALUE, 0, -4, -5};
         int expect_sum = 2147483647;
+        int result = SumPositiveNumberOfArray.calculateSumPositiveNumber(array);
+
+        assertEquals(expect_sum, result);
+    }
+
+    @Test
+    public  void testCalculateSumPositiveNumber4() {
+        int array[] = {-5, -4, -3, -4545, -999};
+        int expect_sum = 0;
         int result = SumPositiveNumberOfArray.calculateSumPositiveNumber(array);
 
         assertEquals(expect_sum, result);
