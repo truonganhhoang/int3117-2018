ad2a9d07ecaf3551a17716ee6ce5629ea1129ce2
Trần Tuấn Anh <trananh9a@gmail.com>
2018-10-29 09:07:40 +0700

    Automatic Test with cypress DO_EXAM

--- cypress/19_DO_EXAM.js
+++ cypress/19_DO_EXAM.js
@@ -1,5 +1,5 @@
 describe('Test Exam', function(){
-  it('Access to website', function(){
+  it('go to test', function(){
     cy.visit('https://hoclieu.sachmem.vn/question_set/5a4b189348150cd8b906539b/5aaf6fd60080c50210e873ff/preview')
   })
 
@@ -9,23 +9,23 @@ describe('Test Exam', function(){
     cy.get(':nth-child(1) > .select-check').click()
     cy.get('.mt-4 > .btn').click()
     cy.get('[title="Câu tiếp"] > .fa').click()
+
     // Cau hoi 2
-    cy.get(':nth-child(1) > .btn-primary').click()
     cy.get(':nth-child(1) > .select-check').click()
     cy.get('.mt-4 > .btn').click()
     cy.get('[title="Câu tiếp"] > .fa').click()
+
     // Cau hoi 3
-    cy.get(':nth-child(1) > .btn-primary').click()
     cy.get(':nth-child(1) > .select-check').click()
     cy.get('.mt-4 > .btn').click()
     cy.get('[title="Câu tiếp"] > .fa').click()
+
     // Cau hoi 4
-    cy.get(':nth-child(1) > .btn-primary').click()
     cy.get(':nth-child(1) > .select-check').click()
     cy.get('.mt-4 > .btn').click()
     cy.get('[title="Câu tiếp"] > .fa').click()
+
     // Cau hoi 5
-    cy.get(':nth-child(1) > .btn-primary').click()
     cy.get(':nth-child(1) > .select-check').click()
     cy.get('.mt-4 > .btn').click()
   })
