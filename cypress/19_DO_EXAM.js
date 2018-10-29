4eefd34757d0f4fb19d79f632253f3dd9487dfa9
Trần Tuấn Anh <trananh9a@gmail.com>
2018-10-29 08:11:13 +0700

    Update 19_DO_EXAM.js

--- cypress/19_DO_EXAM.js
+++ cypress/19_DO_EXAM.js
@@ -3,11 +3,6 @@ describe('Test Exam', function(){
     cy.visit('https://hoclieu.sachmem.vn/question_set/5a4b189348150cd8b906539b/5aaf6fd60080c50210e873ff/preview')
   })
 
-  it('check login', async () => {
-    await cy.get('#link-login').click()
-    await cy.url().should('eq', "https://accounts.sachmem.vn/users/sign_in")
-  })
-
   it('do test', function(){
     cy.get(':nth-child(1) > .btn-primary').click()
     cy.get(':nth-child(1) > .select-check').click()
