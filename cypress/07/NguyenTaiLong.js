46cafae45466ddd64046655e8397c1b7b54a0c1c
tailongnguyen <tailongyoshi@gmail.com>
2018-11-05 11:38:05 +0700

    update cypress code to test on sachmem

--- cypress/07/NguyenTaiLong.js
+++ cypress/07/NguyenTaiLong.js
@@ -1,34 +1,35 @@
 describe('My First Test', function() {
   it('Visits website and check few contents', function() {
-    cy.visit('http://openbookproject.net/thinkcs/python/english3e/index.html')
-    cy.contains('index')
-    cy.contains('next')
-    cy.contains('Chapter') 
+    cy.visit('https://gioithieu.sachmem.vn/')
+    cy.contains('lienhe@sachmem.vn')
+    cy.contains('Đăng nhập')
+    cy.contains('ĐĂNG KÝ') 
   })
 })
 
 describe("My Second Test", function() {
 	it("Click to visit one chapter", function() {
-		cy.visit('http://openbookproject.net/thinkcs/python/english3e/index.html')
-		cy.contains('Chapter 1').click()
-		cy.url().should('include', '.html')
+		cy.visit('https://gioithieu.sachmem.vn/')
+	    cy.contains('Hướng dẫn').click() 
+		cy.url().should('include', '/huong-dan-su-dung/')
 	})
 })
 
 describe("My Third Test", function() {
-	it("Test search engine", function() {
-		cy.visit('http://openbookproject.net/thinkcs/python/english3e/index.html')
-		cy.contains('Search Page').click()
-		cy.get('body > div.document > div.documentwrapper > div > form > input[type="text"]:nth-child(1)').type("list").should("have.value", 'list')
-		cy.get('body > div.document > div.documentwrapper > div > form > input[type="submit"]:nth-child(2)').click()
-		cy.contains('Searching')
+	it("Test search engine by clicking", function() {
+		cy.visit('https://gioithieu.sachmem.vn/huong-dan-su-dung/')
+		cy.contains('Tìm kiếm')
+		cy.get('#epkb_search_terms').type("học máy").should("have.value", 'học máy')
+		cy.get('#epkb-search-kb').click()
+		cy.contains('Kết quả tìm kiếm học máy')
 	})
 })
 
-   // // Should be on a new URL which includes '/commands/actions'
-    // cy.url().should('include', '/commands/actions')
-
-    // // Get an input, type into it and verify that the value has been updated
-    // cy.get('.action-email')
-    //   .type('fake@email.com')
-    //   .should('have.value', 'fake@email.com')
+describe("My Fourth Test", function() {
+	it("Test search engine by pressing keyboard", function() {
+		cy.visit('https://gioithieu.sachmem.vn/huong-dan-su-dung/')
+		cy.contains('Tìm kiếm')
+		cy.get('#epkb_search_terms').type("học máy").type('{enter}')
+		cy.contains('Kết quả tìm kiếm học máy')
+	})
+})
