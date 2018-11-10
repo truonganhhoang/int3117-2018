db250e137c496384d4c759e2c333f0f402c04184
tuananh97 <trananh9a@gmail.com>
2018-11-10 10:46:37 +0700

    Bai tap tuan 11

--- TranTuanAnh/MyMath.java
+++ TranTuanAnh/MyMath.java
@@ -1,19 +1,19 @@
 import java.util.*;
 public class MyMath {
-	public static boolean isPrime(int number) {
-		if (number >= 2) {
-		  if (number % 2 == 0) {
-		    if (number == 2)
-		      return true;
-		  } else {
-		int max = (int) Math.sqrt((double) number);
-		for (int i = 3; i <= max; i += 2) {
-		   if (number % i == 0)
-			  return false;
-			}
-			return true;
-		  }
-		}
-		return false;
-	}
+  public static boolean isPrime(int number) {
+    if (number >= 2) {
+      if (number % 2 == 0) {
+        if (number == 2)
+          return true;
+        } else {
+          int max = (int) Math.sqrt((double) number);
+          for (int i = 3; i <= max; i += 2) {
+            if (number % i == 0)
+              return false;
+          }
+        return true;
+      }
+    }
+  return false;
+  }
 }
