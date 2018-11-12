f16504b0a2ead06f095d5779c6a289d25aabf685
Tran Minh Hieu <35520308+hieutm2198@users.noreply.github.com>
2018-11-12 17:24:44 +0700

    Create ExponentCalculator.java

---
+++ TranMinhHieu/ExponentCalculator/src/ExponentCalculator.java
@@ -0,0 +1,26 @@
+
+public class ExponentCalculator {
+	public static double calculate(double base, double exp) {
+		double result = 0.0;
+		if(base == 0) {
+			if(exp == 0) {
+				result = 1.0;
+			} else {
+				result = 0.0;
+			}
+		} else {
+			if(exp == 0) {
+				result = 1.0;
+			} else {
+				result = base;
+				for(int i = 2; i <= Math.abs(exp); ++i) {
+					result = result * base;
+				}
+				if(exp < 0) {
+					result = 1 / result;
+				}
+			}
+		}
+		return result;
+	}
+}
